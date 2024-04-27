3a0c3b9364347b9ae710167cf10eb81304975318
Mohamed Elkhiat <elkhiat789@gmail.com>
2024-04-27 08:22:21 +0300

    Added PreIndexer Class and some dependencies

--- src/main/java/aurora/search/dev/engine/Indexer/IndexerController.java
+++ src/main/java/aurora/search/dev/engine/Indexer/IndexerController.java
@@ -1,4 +1,44 @@
 package aurora.search.dev.engine.Indexer;
 
+import org.bson.Document;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class IndexerController {
+    public static void main(String[] args) throws IOException {
+        PreIndexer preIndexer = new PreIndexer();
+        // dummy url -> to be changed with url foreach in crawledUrls collection
+        //////////////////////////////////////////////////////////
+        org.jsoup.nodes.Document doc = Jsoup.connect("https://colah.github.io/posts/2015-08-Understanding-LSTMs/").get();
+        org.jsoup.nodes.Document pageDoc = Jsoup.parse(doc.html());
+        Elements allPageElems = pageDoc.getAllElements();
+        String paragraph = "";
+        String elemntName = "", elementText, cleanElemText;
+        for (Element element : allPageElems) {
+
+            elemntName = element.nodeName();
+            if (elemntName.equals("a")) {
+                continue; // skip links
+            }
+            elementText = element.ownText();
+            if (elementText.isEmpty()) {
+                continue; // skip empty text
+            }
+            paragraph = paragraph + elementText + " ";
+        }
+        //////////////////////////////////////////////
+
+        //System.out.println(paragraph);
+        String cleanedPage = preIndexer.cleanPage(paragraph);
+        List<String> tokenizedWords = preIndexer.tokenizeIt(cleanedPage);
+        //System.out.println(tk);
+        List<String> nonStopWords = preIndexer.removeStopWords(tokenizedWords);
+        //System.out.println(tk);
+        List<String> stemWords = preIndexer.StemIt(nonStopWords);
+        //System.out.println(stemWords);
+    }
 }
---
+++ src/main/java/aurora/search/dev/engine/Indexer/PreIndexer.java
@@ -0,0 +1,65 @@
+package aurora.search.dev.engine.Indexer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Vector;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import ca.rmen.porterstemmer.PorterStemmer;
+public class PreIndexer {
+    public  List<String> ReadFile()  {
+        List<String> StopWords = new ArrayList<>();
+        File file = new File("StopWords.txt").getAbsoluteFile();
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String word = scanner.nextLine();
+                StopWords.add(word);
+            }
+        }
+        catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return StopWords;
+    }
+    public  List<String> removeStopWords(List<String> Words)  {
+        List<String> StopWords = ReadFile();
+        Words.removeAll(StopWords);
+        Words.removeIf(item -> item == null || item.isEmpty());
+        return Words;
+    }
+    public String cleanPage(String paragraphToClean){
+        // Removing anything except alphabet characters
+        paragraphToClean = paragraphToClean.replaceAll("<style([\\s\\S]+?)</style>", "");
+        paragraphToClean = paragraphToClean.replaceAll("<script([\\s\\S]+?)</script>", "");
+        paragraphToClean = paragraphToClean.replaceAll("<meta[^>]*>", "");
+        paragraphToClean = paragraphToClean.replaceAll("<link[^>]*>", "");
+        paragraphToClean = paragraphToClean.replaceAll("[^a-zA-Z]", " ");
+
+        return paragraphToClean;
+    }
+    public List<String> tokenizeIt(String sentence){
+        sentence = sentence.toLowerCase();
+        List<String> words =new Vector<>();
+        Pattern pattern = Pattern.compile("\\w+");
+        Matcher match = pattern.matcher(sentence);
+        while (match.find()) {
+            words.add(match.group());
+        }
+        return words;
+    }
+
+    public List<String> StemIt(List<String> words) {
+        PorterStemmer porterStemmer = new PorterStemmer();
+        List<String> stemWords = new ArrayList<>();
+
+        for (String word : words) {
+            String stem = porterStemmer.stemWord(word);
+            stemWords.add(stem);
+        }
+        return stemWords;
+    }
+}
